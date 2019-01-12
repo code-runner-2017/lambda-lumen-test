ae770e983b4d509874e57979cd56a2e443c35b00
Pietro Iglio <igliop@gmail.com>
2019-01-12 23:48:00 +0100

    changed the /testdb endpoint to use lumen database access

--- src/php/lumen/routes/web.php
+++ src/php/lumen/routes/web.php
@@ -19,44 +19,20 @@ $router->get('/hello', function () use ($router) {
 });
 
 $router->get('/test', function (Request $request) use ($router) {
-	$obj = new \stdClass();
-	$obj->title = 'Blade Runner';
-	$obj->director = 'Riddley Scott';
-	$obj->uri = $request->path();
-	$obj->name = $request->input('name');
-	    
-	return response()->json($obj);
+    $obj = new \stdClass();
+    $obj->title = 'Blade Runner';
+    $obj->director = 'Riddley Scott';
+    $obj->uri = $request->path();
+    $obj->name = $request->input('name');
+        
+    return response()->json($obj);
 });
 
 /**
  * Example: Querying an RDS database (postgresql) using plain PDO.
  */
 $router->get('/testdb', function (Request $request) use ($router) {
-	$host = env('DB_HOST');
-	$dbname = env('DB_DATABASE');
-	$username = env('DB_USERNAME');
-	$password = env('DB_PASSWORD');
-	
-	// change this if you want to use MySQL
-	$dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
- 
-	try {
-		$pdo = new \PDO($dsn);
-	
-		$sql = "SELECT user_id, login_name, email FROM users";
-		$result = $pdo->query($sql);
-				
-        $users = [];
-		
-        while ($row = $result->fetch(\PDO::FETCH_ASSOC)) {
-            $users[] = $row;
-        }
-		
-		return response()->json($users);
-	
-	}catch (PDOException $e){
-		// report error message
-		return $e->getMessage();
-	}
-
+    $users = app('db')->select("SELECT * FROM users");    
+    return response()->json($users);    
 });
+
